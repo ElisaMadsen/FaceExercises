9093207eb1948dcf14cf6c415d286c87a790465b
JohannesForsting98 <johannes.forsting@gmail.com>
2021-09-08 10:58:16 +0200

    Tilføjede lige noget

--- src/FaceExercises.java
+++ src/FaceExercises.java
@@ -1,6 +1,7 @@
 public class FaceExercises {
     public static void main (String[] args) {
         System.out.println("Hej");
+        System.out.println("Hej Elisa, jeg har ændret din kode");
 
     }
 }
