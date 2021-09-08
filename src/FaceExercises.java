712d1c9e030639151676f4f79edce93549761340
JohannesForsting98 <johannes.forsting@gmail.com>
2021-09-08 11:22:31 +0200

    Test

--- src/FaceExercises.java
+++ src/FaceExercises.java
@@ -2,6 +2,7 @@ public class FaceExercises {
     public static void main (String[] args) {
         System.out.println("Hej");
         System.out.println("Hej Elisa, jeg har ændret din kode");
+        System.out.println("test123");
 
     }
 }
